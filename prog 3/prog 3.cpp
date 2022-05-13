24f74eee14c35287460cf146836eb0774e11bec8
EugeneFreak <zeny-medved@mail.ru>
2022-05-13 16:39:41 +0300

    Добавьте файлы проекта.

---
+++ prog 3/prog 3.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+using namespace std;
+int power(int, unsigned);
+
+int main()
+{
+    int a;
+    int b;
+    cin >> a;
+    cin >> b;
+   
+    cout << power(a, b);
+    
+}
+
+int power(int x, unsigned p) {
+    int answer;
+    answer = x;
+    /* считаем answer */
+    if (p == 0) {
+        return 1;
+    }
+    else if (p == 1) {
+        return x;
+    }
+    else {
+        for (int i = 1; i < p; i++) {
+            answer = answer * x;
+        }
+    }
+    return answer;
+}
